730943707e40699eeabb24159449a8eae4f371d9
Dimitri Vorona <vorona@in.tum.de>
2018-09-07 13:46:34 +0200

    Add task definition

---
+++ db/tasks.php
@@ -0,0 +1,12 @@
+<?php
+$tasks = array(
+    array(
+        'classname' => 'mod_digitalization\task\check_deliveries',
+        'blocking' => 0,
+        'minute' => '*/15',
+        'hour' => '*',
+        'day' => '*',
+        'dayofweek' => '*',
+        'month' => '*'
+    )
+);
